ebe45cf7713d4474d59896acf44cf6d5c606de00
andthum <56444687+andthum@users.noreply.github.com>
2023-03-15 16:21:22 +0100

    [plot.py]: Add `offset` Option for Plotting Electrodes

--- src/lintf2_ether_ana_postproc/plot.py
+++ src/lintf2_ether_ana_postproc/plot.py
@@ -12,7 +12,7 @@ atom_type2display_name = {
 }
 
 
-def plot_elctrd_left(ax, **kwargs):
+def plot_elctrd_left(ax, offset=0, **kwargs):
     """
     Plot the position of the electrode layers of the left electrode as
     vertical lines into an :class:`matplotlib.axes.Axes`.
@@ -22,6 +22,9 @@ def plot_elctrd_left(ax, **kwargs):
     ax : matplotlib.axes.Axes
         The :class:`~matplotlib.axes.Axes` into which to plot the
         electrode positions.
+    offset : float, optional
+        x value at which to position the rightmost layer of the left
+        electrode.
     kwargs : dict
         Keyword arguments to parse to
         :func:`matplotlib.axes.Axes.axvline`.  See there for possible
@@ -32,13 +35,13 @@ def plot_elctrd_left(ax, **kwargs):
     kwargs.setdefault("linestyle", "dashed")
 
     Elctrd = leap.simulation.Electrode()
-    elctrd_pos_z = 0
+    elctrd_pos_z = offset
     for _ in range(Elctrd.GRA_LAYERS_N):
         ax.axvline(x=elctrd_pos_z, **kwargs)
-        elctrd_pos_z += Elctrd.GRA_LAYER_DIST / 10  # nm -> Angstrom
+        elctrd_pos_z -= Elctrd.GRA_LAYER_DIST / 10  # nm -> Angstrom
 
 
-def plot_elctrd_right(ax, box_z, **kwargs):
+def plot_elctrd_right(ax, offset=0, **kwargs):
     """
     Plot the position of the electrode layers of the right electrode as
     vertical lines into an :class:`matplotlib.axes.Axes`.
@@ -48,8 +51,9 @@ def plot_elctrd_right(ax, box_z, **kwargs):
     ax : matplotlib.axes.Axes
         The :class:`~matplotlib.axes.Axes` into which to plot the
         electrode positions.
-    box_z : float
-        Length of the simulation box in z direction in nanometers.
+    offset : float, optional
+        x value at which to position the leftmost layer of the right
+        electrode.
     kwargs : dict
         Keyword arguments to parse to
         :func:`matplotlib.axes.Axes.axvline`.  See there for possible
@@ -60,13 +64,13 @@ def plot_elctrd_right(ax, box_z, **kwargs):
     kwargs.setdefault("linestyle", "dashed")
 
     Elctrd = leap.simulation.Electrode()
-    elctrd_pos_z = box_z
+    elctrd_pos_z = offset
     for _ in range(Elctrd.GRA_LAYERS_N):
         ax.axvline(x=elctrd_pos_z, **kwargs)
-        elctrd_pos_z -= Elctrd.GRA_LAYER_DIST / 10  # nm -> Angstrom
+        elctrd_pos_z += Elctrd.GRA_LAYER_DIST / 10  # nm -> Angstrom
 
 
-def plot_elctrds(ax, box_z, **kwargs):
+def plot_elctrds(ax, offset_right, offset_left=0, **kwargs):
     """
     Plot the position of the electrode layers of the left and right
     electrode as vertical lines into an :class:`matplotlib.axes.Axes`.
@@ -76,8 +80,9 @@ def plot_elctrds(ax, box_z, **kwargs):
     ax : matplotlib.axes.Axes
         The :class:`~matplotlib.axes.Axes` into which to plot the
         electrode positions.
-    box_z : float
-        Length of the simulation box in z direction in nanometers.
+    offset_left, offset_right : float
+        x values at which to position the rightmost layer of the left
+        electrode and the leftmost layer of the right electrode.
     kwargs : dict
         Keyword arguments to parse to
         :func:`lintf2_ether_ana_postproc.plot.plot_elctrd_left` and
@@ -87,5 +92,5 @@ def plot_elctrds(ax, box_z, **kwargs):
     """
     kwargs.setdefault("color", "tab:gray")
     kwargs.setdefault("linestyle", "dashed")
-    leap.plot.plot_elctrd_left(ax, **kwargs)
-    leap.plot.plot_elctrd_right(ax, box_z, **kwargs)
+    leap.plot.plot_elctrd_left(ax, offset_left, **kwargs)
+    leap.plot.plot_elctrd_right(ax, offset_right, **kwargs)
