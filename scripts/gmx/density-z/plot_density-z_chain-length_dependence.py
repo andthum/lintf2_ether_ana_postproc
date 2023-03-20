f41b1ce4415f656b0be7c7f31d5e3c28d119ed32
andthum <56444687+andthum@users.noreply.github.com>
2023-03-20 14:05:42 +0100

    [plot_density-z_chain-length_dependence.py]: Add Option `--common-ymax`
    * Add Option `--common-ymax`.
    
    * Rename Option `-q` to `--surfq`.
    
    * Add "r = 0.05" to legend title.
    
    * Change common `ymax` from 6.25 to 6.5

--- scripts/gmx/density-z/plot_density-z_chain-length_dependence.py
+++ scripts/gmx/density-z/plot_density-z_chain-length_dependence.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-"""Plot density profiles"""
+"""Plot density profiles for different chain lengths."""
 
 
 # Standard libraries
@@ -25,15 +25,21 @@ parser = argparse.ArgumentParser(
     description="Plot density profiles for different chain lengths."
 )
 parser.add_argument(
-    "-q",
+    "--surfq",
     type=str,
     required=True,
     choices=("q0", "q0.25", "q0.5", "q0.75", "q1"),
     help="Surface charge in e/nm^2.",
 )
+parser.add_argument(
+    "--common-ymax",
+    required=False,
+    default=False,
+    action="store_true",
+    help="Use common y limits for all plots.",
+)
 args = parser.parse_args()
 
-common_ymax = True
 settings = "pr_nvt423_nh"  # Simulation settings.
 analysis = "density-z"  # Analysis name.
 analysis_suffix = "_number"  # Analysis name specification.
@@ -41,12 +47,12 @@ tool = "gmx"  # Analysis software.
 outfile = (  # Output file name.
     settings
     + "_lintf2_peoN_20-1_gra_"
-    + args.q
+    + args.surfq
     + "_sc80_"
     + analysis
     + analysis_suffix
 )
-if common_ymax:
+if args.common_ymax:
     outfile += "_common_ymax.pdf"
 else:
     outfile += ".pdf"
@@ -68,10 +74,10 @@ if len(compounds) != len(cols):
 
 print("Creating Simulation instances...")
 SimPaths = leap.simulation.SimPaths()
-pattern_system = "lintf2_*[0-9]*_20-1_gra_q[0-9]*_sc80"
+pattern_system = "lintf2_[gp]*[0-9]*_20-1_gra_" + args.surfq + "_sc80"
 pattern_settings = "[0-9][0-9]_" + settings + "_" + pattern_system
 pattern = os.path.join(
-    SimPaths.PATHS[args.q], pattern_system, pattern_settings
+    SimPaths.PATHS[args.surfq], pattern_system, pattern_settings
 )
 paths = glob.glob(pattern)
 Sims = leap.simulation.Simulations(*paths, sort_key="O_per_chain")
@@ -92,36 +98,38 @@ n_infiles = len(infiles)
 print("Reading data and creating plot(s)...")
 Elctrd = leap.simulation.Electrode()
 elctrd_thk = Elctrd.ELCTRD_THK / 10  # A -> nm
+box_z_max = np.max(Sims.boxes_z)
 
 plot_sections = ("left", "right", "full")
 # xmin = -elctrd_thk
 xmin = 0
 xmax = 4
-if args.q == "q0":
-    ymax = tuple((6.25, 4, 3, 3) for _ in plot_sections)
-elif args.q == "q0.25":
-    ymax = ((4, 7, 10.5, 2.8), (12.5, 2, 1.8, 4.8))
+if args.surfq == "q0":
+    ymax = tuple((6.25, 4, 3.2, 3.2) for _ in plot_sections)
+elif args.surfq == "q0.25":
+    ymax = ((4, 7, 10.5, 2.8), (12.5, 2.1, 2.1, 4.8))
     ymax += (tuple(np.max(ymax, axis=0)),)
-elif args.q == "q0.5":
-    ymax = ((4.2, 11.5, 20, 4), (25, 3.1, 2, 6))
+elif args.surfq == "q0.5":
+    ymax = ((4.2, 11.5, 20, 4), (25, 3.1, 2.1, 6))
     ymax += (tuple(np.max(ymax, axis=0)),)
-elif args.q == "q0.75":
+elif args.surfq == "q0.75":
     ymax = ((4, 23, 36, 4.6), (23, 4.6, 3.1, 7))
     ymax += (tuple(np.max(ymax, axis=0)),)
-elif args.q == "q1":
+elif args.surfq == "q1":
     ymax = ((4.6, 46, 62.5, 4.6), (82.5, 4.6, 4.6, 6.25))
     ymax += (tuple(np.max(ymax, axis=0)),)
 else:
-    raise ValueError("Unknown surface charge -q: '{}'".format(args.q))
-if common_ymax:
+    raise ValueError("Unknown surface charge --surfq: '{}'".format(args.surfq))
+if args.common_ymax:
     # ymax = tuple(
     #     tuple(None for _cmp in compounds)
     #     for _plt_sec in plot_sections
     # )
     ymax = tuple(
-        tuple(6.25 for _cmp in compounds) for _plt_sec in plot_sections
+        tuple(6.5 for _cmp in compounds) for _plt_sec in plot_sections
     )
 
+linewidth = 1.5
 cmap = plt.get_cmap()
 mdt.fh.backup(outfile)
 with PdfPages(outfile) as pdf:
@@ -136,7 +144,7 @@ with PdfPages(outfile) as pdf:
                 if plt_sec in ("left", "right"):
                     # Also, for the plot of the right electrode, the
                     # electrode position will be shifted to zero.
-                    leap.plot.plot_elctrd_left(ax)
+                    leap.plot.plot_elctrd_left(ax, linewidth=linewidth)
 
                 for sim_ix, Sim in enumerate(Sims.sims):
                     x, y = np.loadtxt(
@@ -155,13 +163,19 @@ with PdfPages(outfile) as pdf:
                         bulk_dens = Sim.dens["atm_type"][cmp]["num"]
                         bulk_dens *= 1e3  # 1/A^3 -> 1/nm^3
                         y /= bulk_dens
-                    if args.q == "q0" and Sim.O_per_chain == 6 and cmp == "Li":
+
+                    if (
+                        args.surfq == "q0"
+                        and Sim.O_per_chain == 6
+                        and cmp == "Li"
+                    ):
                         color = "tab:red"
                         ax.plot([], [])  # Increment color cycle.
                     elif (
-                        args.q == "q1"
+                        args.surfq == "q1"
                         and Sim.O_per_chain == 2
                         and cmp in ("NBT", "OBT")
+                        and plt_sec == "right"
                     ):
                         color = "tab:red"
                         ax.plot([], [])  # Increment color cycle.
@@ -170,9 +184,9 @@ with PdfPages(outfile) as pdf:
                     ax.plot(
                         x,
                         y,
-                        label=str(Sim.O_per_chain),
+                        label=r"$%d$" % Sim.O_per_chain,
                         color=color,
-                        linewidth=1.5,
+                        linewidth=linewidth,
                         alpha=2 / 3,
                     )
 
@@ -199,30 +213,32 @@ with PdfPages(outfile) as pdf:
                     # Reverse x-axis.
                     xlim = (xmax, xmin)
                 else:
-                    xlabel = r"z / nm"
-                    xlim = (0, np.max(Sims.boxes_z))
+                    xlabel = r"$z$ / nm"
+                    xlim = (0, box_z_max)
                 ax.set(xlabel=xlabel, ylabel=ylabel, xlim=xlim, ylim=ylim)
 
                 legend_title = (
                     r"%.2f$" % Sims.surfqs[0]
                     + r" $e$/nm$^2$"
                     + "\n"
+                    + r"$r = %.2f$" % Sims.Li_O_ratios[0]
+                    + "\n"
                     + r"$n_{EO}$"
                 )
                 if plt_sec == "left":
-                    if args.q == "q0":
+                    if args.surfq == "q0":
                         legend_title = r"$\sigma_s = " + legend_title
                     else:
                         legend_title = r"$\sigma_s = +" + legend_title
                     legend_loc = "upper right"
                 elif plt_sec == "right":
-                    if args.q == "q0":
+                    if args.surfq == "q0":
                         legend_title = r"$\sigma_s = " + legend_title
                     else:
                         legend_title = r"$\sigma_s = -" + legend_title
                     legend_loc = "upper left"
                 else:
-                    if args.q == "q0":
+                    if args.surfq == "q0":
                         legend_title = r"$\sigma_s = " + legend_title
                     else:
                         legend_title = r"$\sigma_s = \pm" + legend_title
@@ -238,4 +254,5 @@ with PdfPages(outfile) as pdf:
                 pdf.savefig()
                 plt.close()
 
+print("Created {}".format(outfile))
 print("Done")
