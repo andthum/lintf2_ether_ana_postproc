181cd1fc290286c03e6227d9cb431a34ccbe31c8
andthum <56444687+andthum@users.noreply.github.com>
2023-10-31 16:59:20 +0100

    [get_diff_coeff.py]: Create (WIP)

--- scripts/bulk_and_walls/mdt/msd/get_diff_coeff.py
+++ scripts/bulk_and_walls/mdt/msd/get_diff_coeff.py
@@ -3,12 +3,19 @@
 # python3 -m pip install --upgrade pynumdiff CVXPY PYCHEBFUN
 
 """
+.. warning::
+
+    This script is work in progress! The hyperparameters for finding the
+    diffusive regime need further fine tuning.
+
 Identify the diffusive regime of the mean squared displacement (MSD) and
 extract the self-diffusion coefficient by fitting a straight line to it.
 """
 
 
 # Standard libraries
+import argparse
+import logging
 import os
 import sys
 from datetime import datetime, timedelta
@@ -25,6 +32,11 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 # First-party libraries
 import lintf2_ether_ana_postproc as leap
+
+
+# pynumdiff produces logs by default.  The following line effectively
+# disables all logging calls.
+logging.disable(logging.CRITICAL)
 
 
 def einstein_msd(times, diff_coeff, n_dim=3):
@@ -109,11 +121,73 @@ proc = psutil.Process()
 proc.cpu_percent()  # Initiate monitoring of CPU usage.
 
 # Input parameters.
-cmp = "Li"
-infile = "pr_nvt423_nh_lintf2_peo63_20-1_sc80_msd_" + cmp + ".txt.gz"
-outfile = "pr_nvt423_nh_lintf2_peo63_20-1_sc80_msd_" + cmp + ".pdf"
-# Number of dimensions in which the diffusive process takes place.
-n_dim = 3
+parser = argparse.ArgumentParser(
+    description=(
+        "Identify the diffusive regime of the mean squared displacement (MSD)"
+        " and extract the self-diffusion coefficient by fitting a straight"
+        " line to it."
+    )
+)
+parser.add_argument(
+    "--system",
+    type=str,
+    required=True,
+    help="Name of the simulated system, e.g. lintf2_g1_20-1_gra_q1_sc80.",
+)
+parser.add_argument(
+    "--settings",
+    type=str,
+    required=False,
+    default="pr_nvt423_nh",
+    help=(
+        "String describing the used simulation settings.  Default:"
+        " %(default)s."
+    ),
+)
+parser.add_argument(
+    "--cmp",
+    type=str,
+    required=False,
+    default="Li",
+    choices=("Li", "NTf2", "ether", "NBT", "OBT", "OE"),
+    help="Compound.  Default: %(default)s",
+)
+parser.add_argument(
+    "--msd-component",
+    type=str,
+    required=False,
+    default="tot",
+    choices=("tot", "x", "y", "z"),
+    help="The MSD component to use for the analysis.  Default: %(default)s",
+)
+args = parser.parse_args()
+
+analysis = "msd"  # Analysis name.
+tool = "mdt"  # Analysis software.
+outfile_base = (
+    args.settings + "_" + args.system + "_" + analysis + "_" + args.cmp
+)
+outfile_txt = outfile_base + ".txt.gz"
+outfile_pdf = outfile_base + ".pdf"
+
+# Columns to read from the MSD file.
+cols = (0,)  # Diffusion times [ps].
+if args.msd_component == "tot":
+    cols += (1,)
+    # Number of dimensions in which the diffusive process takes place.
+    n_dim = 3
+elif args.msd_component == "x":
+    cols += (2,)
+    n_dim = 1
+elif args.msd_component == "y":
+    cols += (3,)
+    n_dim = 1
+elif args.msd_component == "z":
+    cols += (4,)
+    n_dim = 1
+else:
+    raise ValueError("Unknown --msd-component ({})".format(args.msd_component))
+
 # Number of frames between restarting points used for calculating the
 # MSD.
 restart_interval = 500  # [frames].
@@ -121,22 +195,34 @@ restart_interval = 500  # [frames].
 restart_frequency = 1 / (restart_interval * 2e-3)  # [ns^-1].
 # Cutoff frequency for estimating `tvgamma`.
 cutoff_frequency = restart_frequency / 2  # [ns^-1].
-# Optimize the smoothing parameters using the MSD/t data between
-# `start_opt_pct` and `end_opt_pct` percent of the data.
-start_opt_pct = 0.0
-end_opt_pct = 0.9
 # Calculate the standard deviation of the derivative of MSD/t between
-# `start_deriv_sd_pct` and `end_deriv_sd_pct` percent of the data.
+# `start_deriv_sd_pct` and `stop_deriv_sd_pct` percent of the data.
 start_deriv_sd_pct = 0.8
-end_deriv_sd_pct = end_opt_pct
+stop_deriv_sd_pct = 0.9
 # Regard the derivative of MSD/t as zero if it lies within +/- the
 # calculated standard deviation times `deriv_sd_factor`.
 deriv_sd_factor = 4
 
 
+print("Creating Simulation instance(s)...")
+timer = datetime.now()
+if "_gra_" in args.system:
+    surfq = leap.simulation.get_surfq(args.system)
+    top_path = "q%g" % surfq
+else:
+    surfq = None
+    top_path = "bulk"
+set_pat = "[0-9][0-9]_" + args.settings + "_" + args.system
+Sim = leap.simulation.get_sim(args.system, set_pat, top_path)
+print("Elapsed time:         {}".format(datetime.now() - timer))
+print("Current memory usage: {:.2f} MiB".format(mdt.rti.mem_usage(proc)))
+
+
 print("\n")
 print("Reading data...")
 timer = datetime.now()
+file_suffix = analysis + "_" + args.cmp + ".txt.gz"
+infile = leap.simulation.get_ana_file(Sim, analysis, tool, file_suffix)
 times, msd = np.loadtxt(infile, usecols=[0, 1], unpack=True)
 times *= 1e-3  # ps -> ns
 msd *= 1e-2  # A^2 -> nm^2
@@ -162,23 +248,18 @@ print("\n")
 print("Calculating optimized smoothing parameters...")
 timer = datetime.now()
 tvgamma = estimate_tvgamma(cutoff_frequency=cutoff_frequency, dt=time_diff)
+tvgamma *= 100
 # Initial guesses of the smoothing parameters.
 param_guesses = [
     [
         1,  # Order of the polynomial.
-        restart_interval // 5,  # Size of the sliding window.
-        restart_interval // 5,  # Window size for Gaussian smoothing.
+        restart_interval // n,  # Size of the sliding window.
+        restart_interval // n,  # Window size for Gaussian smoothing.
     ]
+    for n in (0.1, 0.2, 1, 5, 10)
 ]
-# Because the optimization is slow, only run it on the most relevant
-# part of the data (from `start_opt_tot` to `end_opt_tot`).
-start_opt_tot = int(start_opt_pct * len(msd_t))
-end_opt_tot = int(end_opt_pct * len(msd_t))
 params, val = pynumdiffopt.linear_model.savgoldiff(
-    msd_t[start_opt_tot:end_opt_tot],
-    dt=time_diff,
-    params=param_guesses,
-    tvgamma=tvgamma,
+    msd_t, dt=time_diff, params=param_guesses, tvgamma=tvgamma
 )
 print("tvgamma = {}".format(tvgamma))
 print("params  = {}".format(params))
@@ -201,15 +282,15 @@ print("\n")
 print("Identifying diffusive regime and calculating diffusion coefficient...")
 timer = datetime.now()
 start_deriv_sd_tot = int(start_deriv_sd_pct * len(msd_t_deriv))
-end_deriv_sd_tot = int(end_deriv_sd_pct * len(msd_t_deriv))
+stop_deriv_sd_tot = int(stop_deriv_sd_pct * len(msd_t_deriv))
 msd_t_deriv_sd = np.std(
-    msd_t_deriv[start_deriv_sd_tot:end_deriv_sd_tot], ddof=1
+    msd_t_deriv[start_deriv_sd_tot:stop_deriv_sd_tot], ddof=1
 )
 deriv_tol = deriv_sd_factor * msd_t_deriv_sd
 diffusive = (msd_t_deriv > -deriv_tol) & (msd_t_deriv < deriv_tol)
 if not np.any(diffusive):
     fit_start, fit_stop = -1, -1
-    diff_coeff = np.nan
+    diff_coeff, diff_coeff_sd = np.nan, np.nan
     times_fit, msd_fit = np.array([]), np.array([])
 else:
     first_true = np.argmax(diffusive)
@@ -219,8 +300,18 @@ else:
     fit_start += first_true
     fit_stop = fit_start + length
     diff_coeff = np.mean(msd_t[fit_start:fit_stop]) / (2 * n_dim)
+    diff_coeff_sd = np.std(msd_t[fit_start:fit_stop], ddof=1)
+    diff_coeff_sd /= 2 * n_dim
     times_fit = times_t[fit_start:fit_stop]
     msd_fit = einstein_msd(times_fit, diff_coeff, n_dim)
+print("Elapsed time:         {}".format(datetime.now() - timer))
+print("Current memory usage: {:.2f} MiB".format(mdt.rti.mem_usage(proc)))
+
+
+print("\n")
+print("Creating output...")
+timer = datetime.now()
+header = ""
 print("Elapsed time:         {}".format(datetime.now() - timer))
 print("Current memory usage: {:.2f} MiB".format(mdt.rti.mem_usage(proc)))
 
@@ -230,7 +321,7 @@ print("Creating plots...")
 timer = datetime.now()
 label_orig = "Original"
 label_smooth = "Smoothed"
-label_fit = "Fit of Orig."
+label_fit = "Fit"
 color_orig = "tab:blue"
 color_smooth = "tab:orange"
 color_fit = "black"
@@ -242,8 +333,8 @@ lw_orig = None
 lw_smooth = None
 lw_fit = None
 
-mdt.fh.backup(outfile)
-with PdfPages(outfile) as pdf:
+mdt.fh.backup(outfile_pdf)
+with PdfPages(outfile_pdf) as pdf:
     # Plot MSD vs time.
     fig, ax = plt.subplots(clear=True)
     ax.plot(
@@ -358,6 +449,15 @@ with PdfPages(outfile) as pdf:
         label="Fit Region",
         alpha=leap.plot.ALPHA,
     )
+    ax.axvline(
+        times_t[start_deriv_sd_tot],
+        color="tab:green",
+        label="Std Region",
+        alpha=leap.plot.ALPHA,
+    )
+    ax.axvline(
+        times_t[stop_deriv_sd_tot], color="tab:green", alpha=leap.plot.ALPHA
+    )
     ax.set(
         xlabel=r"Diffusion Time $t$ / ns",
         ylabel=r"d$/$d$t$ MSD$(t)/t$ / nm$^2$/ns$^2$",
@@ -371,13 +471,13 @@ with PdfPages(outfile) as pdf:
     # Linear scale, zoom to 0 +/- 2*tolerance.
     ax.axhline(
         -deriv_tol,
-        label="Tolerance",
+        label=r"Tolerance ($%.1f \sigma$)" % deriv_sd_factor,
         color=color_thresh,
         alpha=leap.plot.ALPHA,
     )
     ax.axhline(deriv_tol, color=color_thresh, alpha=leap.plot.ALPHA)
     ax.set_xscale("linear")
-    ax.set_ylim(-2 * deriv_tol, 2 * deriv_tol)
+    ax.set_ylim(-3 * deriv_tol, 3 * deriv_tol)
     ax.legend(loc="upper left")
     pdf.savefig()
     # Log scale x.
@@ -406,9 +506,14 @@ with PdfPages(outfile) as pdf:
     ax.legend(loc="upper right")
     pdf.savefig()
     plt.close()
-print("Created {}".format(outfile))
+print("Created {}".format(outfile_pdf))
 print("Elapsed time:         {}".format(datetime.now() - timer))
 print("Current memory usage: {:.2f} MiB".format(mdt.rti.mem_usage(proc)))
+
+# Remove the log file created by default by pynumdiff.
+pynumdiff_debug_log = "debug.log"
+if os.path.isfile(pynumdiff_debug_log):
+    os.remove(pynumdiff_debug_log)
 
 print("\n")
 print("{} done".format(os.path.basename(sys.argv[0])))
