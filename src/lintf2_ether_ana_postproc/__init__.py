c19b08703fd1f4ea8b919275e61c7dda9e5c074d
andthum <56444687+andthum@users.noreply.github.com>
2023-11-18 12:47:38 +0100

    [__init__.py]: Bump Version Number from `0.2.0` to `0.3.0`

--- src/lintf2_ether_ana_postproc/__init__.py
+++ src/lintf2_ether_ana_postproc/__init__.py
@@ -11,9 +11,10 @@ from . import clstr, io_handler, misc, plot, simulation, transfer
 
 __all__ = ["clstr", "io_handler", "misc", "plot", "simulation", "transfer"]
 
-# Project metadata.  Keep in sync with `pyproject.toml`!
+# Project metadata.
+# Keep in sync with `pyproject.toml` and `CITATION.cff`!
 __title__ = "lintf2_ether_ana_postproc"
-__version__ = "0.2.0"  # Keep in sync with `pyproject.toml`!
+__version__ = "0.3.0"  # Keep in sync with `pyproject.toml`!
 __author__ = "Andreas Thum"
 __email__ = "coding.andthum@e.mail.de"
 __license__ = "MIT License"
