ea5e19bac9ab874e91b6bff6759373d05c290cca
andthum <56444687+andthum@users.noreply.github.com>
2023-12-14 00:22:01 +0100

    [plot_bin_lifetime_hist_chain-length_dependence.py]: Create (WIP)

--- scripts/bulk_and_walls/mdt/discrete-z/plot_bin_lifetime_hist_chain-length_dependence.py
+++ scripts/bulk_and_walls/mdt/discrete-z/plot_bin_lifetime_hist_chain-length_dependence.py
@@ -9,6 +9,7 @@ bins for various chain lengths.
 
 # Standard libraries
 import argparse
+import warnings
 
 # Third-party libraries
 import mdtools as mdt
@@ -21,7 +22,42 @@ from matplotlib.backends.backend_pdf import PdfPages
 import lintf2_ether_ana_postproc as leap
 
 
-def histograms(dtrj_file, uncensored=False, intermittency=0):
+def legend_title(surfq_sign, position):
+    r"""
+    Create a legend title string.
+
+    Parameters
+    ----------
+    surfq_sign : {"+", "-", r"\pm"}
+        The sign of the surface charge.
+
+    Returns
+    -------
+    title : str
+        The legend title.
+
+    Notes
+    -----
+    This function relies on global variables!
+    """
+    if surfq_sign not in ("+", "-", r"\pm"):
+        raise ValueError("Unknown `surfq_sign`: '{}'".format(surfq_sign))
+    title = (
+        r"$\sigma_s = "
+        + surfq_sign
+        + r" %.2f$ $e$/nm$^2$, " % Sims.surfqs[0]
+        + r"$r = %.2f$" % Sims.Li_O_ratios[0]
+        + "\n"
+    )
+    if position == "bulk":
+        title += "Bulk\n"
+    else:
+        title += r"Dist. to Electrode: $\sim %.2f$ nm" % position + "\n"
+    title += r"$n_{EO}$"
+    return title
+
+
+def histograms(dtrj_file, uncensored=False, intermittency=0, time_conv=1):
     """TODO"""
     # Read discrete trajectory.
     dtrj = mdt.fh.load_dtrj(dtrj_file)
@@ -40,6 +76,8 @@ def histograms(dtrj_file, uncensored=False, intermittency=0):
     )
     states = states.astype(np.uint16)
     n_states = len(states)
+    if n_states == 0:
+        raise ValueError("`n_states` == 0")
     del dtrj
 
     # Calculate lifetime histogram for each state.
@@ -70,6 +108,7 @@ def histograms(dtrj_file, uncensored=False, intermittency=0):
                 " one".format(np.sum(hists[state_ix] * np.diff(bins)))
             )
     del lts_per_state, lts_state, _bins
+    bins *= time_conv
     return hists, bins.astype(np.float32), states
 
 
@@ -180,9 +219,8 @@ hists_bins = [None for sim in Sims.sims]
 # Lifetime histograms in the bulk and for each layer/free-energy minimum
 hists_bulk = [None for sim in Sims.sims]
 hists_layer = [[None for sim in Sims.sims] for pkp_type in pk_pos_types]
-hists_layer_state_ix = [
-    [None for sim in Sims.sims] for pkp_type in pk_pos_types
-]
+hist_layer_states = [[None for sim in Sims.sims] for pkp_type in pk_pos_types]
+n_states_max = [0 for pkp_type in pk_pos_types]
 # Lower and upper bin edges used for generating the discrete trajectory
 # -> Free-energy maxima.
 # Don't confuse position bins (used to generate the discrete trajectory)
@@ -191,20 +229,19 @@ hists_layer_state_ix = [
 bin_edges = [[None for sim in Sims.sims] for pkp_type in pk_pos_types]
 # Data to be clustered: Bin midpoints, Simulation indices, bin indices.
 n_data_clstr = 3
+pkp_col_ix = 0
 data_clstr = [
     [[None for sim in Sims.sims] for pkp_type in pk_pos_types]
     for col_ix in range(n_data_clstr)
 ]
 for sim_ix, Sim in enumerate(Sims.sims):
-    box_z = Sim.box[2] / 10  # A -> nm
-    bulk_region = Sim.bulk_region / 10  # A -> nm
-
     # Calculate the lifetime histogram for each state from the discrete
     # trajectory.
     hists_sim, hists_bins_sim, states_sim = histograms(
         infiles_dtrj[sim_ix],
         uncensored=args.uncensored,
         intermittency=args.intermittency,
+        time_conv=time_conv,
     )
     hists_bins[sim_ix] = hists_bins_sim
     hists_bulk[sim_ix] = hists_sim[len(states_sim) // 2]
@@ -228,6 +265,7 @@ for sim_ix, Sim in enumerate(Sims.sims):
     del bin_edges_lower, bin_edges_upper, bin_mids
     # Select all bins for which lifetime histograms are available.
     bin_data = bin_data[states_sim]
+    box_z = Sim.box[2] / 10  # A -> nm
     bin_is_left = bin_data[:, -1] <= (box_z / 2)
     tolerance = 1e-6
     if np.any(bin_data <= -tolerance):
@@ -284,11 +322,12 @@ for sim_ix, Sim in enumerate(Sims.sims):
             )
 
         hists_layer[pkt_ix][sim_ix] = hists_sim_valid
-        hists_layer_state_ix[pkt_ix][sim_ix] = states_sim_valid
+        hist_layer_states[pkt_ix][sim_ix] = states_sim_valid
         bin_edges[pkt_ix][sim_ix] = bin_data_valid[:, :2]
 
         n_states_valid = len(states_sim_valid)
-        data_clstr[0][pkt_ix][sim_ix] = bin_data_valid[:, -1]
+        n_states_max[pkt_ix] = max(n_states_valid, n_states_max[pkt_ix])
+        data_clstr[pkp_col_ix][pkt_ix][sim_ix] = bin_data_valid[:, -1]
         data_clstr[1][pkt_ix][sim_ix] = np.full(
             n_states_valid, sim_ix, dtype=np.uint16
         )
@@ -296,3 +335,168 @@ for sim_ix, Sim in enumerate(Sims.sims):
 
 
 print("Clustering peak positions...")
+(
+    data_clstr,
+    clstr_ix,
+    linkage_matrices,
+    n_clstrs,
+    n_pks_per_sim,
+    clstr_dist_thresh,
+) = leap.clstr.peak_pos(data_clstr, pkp_col_ix, return_dist_thresh=True)
+clstr_ix_unq = [np.unique(clstr_ix_pkt) for clstr_ix_pkt in clstr_ix]
+
+# Sort clusters by ascending average peak position.
+for pkt_ix, clstr_ix_pkt in enumerate(clstr_ix):
+    _clstr_dists, clstr_ix[pkt_ix] = leap.clstr.dists_succ(
+        data_clstr[pkp_col_ix][pkt_ix],
+        clstr_ix_pkt,
+        method=clstr_dist_method,
+        return_ix=True,
+    )
+
+if np.any(n_clstrs < n_states_max):
+    warnings.warn(
+        "Any `n_clstrs` ({}) < `n_states_max` ({}).  This means different"
+        " bins of the same simulation were assigned to the same cluster."
+        "  Try to decrease the threshold distance"
+        " ({})".format(n_clstrs, n_states_max, clstr_dist_thresh),
+        RuntimeWarning,
+        stacklevel=2,
+    )
+
+
+print("Creating plot(s)...")
+xlabel = "Residence Time / ns"
+ylabel = "PDF"
+xlim = (time_conv, 1e3)
+ylim = (None, None)
+
+n_legend_cols = 1 + Sims.n_sims // (4 + 1)
+
+cmap = plt.get_cmap()
+c_vals = np.arange(Sims.n_sims)
+c_norm = max(1, Sims.n_sims - 1)
+c_vals_normed = c_vals / c_norm
+colors = cmap(c_vals_normed)
+
+mdt.fh.backup(outfile)
+with PdfPages(outfile) as pdf:
+    # Plot lifetime histograms for bulk bin.
+    fig, ax = plt.subplots(clear=True)
+    ax.set_prop_cycle(color=colors)
+    for sim_ix, hist in enumerate(hists_bulk):
+        ax.stairs(
+            hist,
+            hists_bins[sim_ix],
+            fill=False,
+            label=r"$%d$" % Sims.O_per_chain[sim_ix],
+            alpha=leap.plot.ALPHA,
+            rasterized=False,
+        )
+        # Use for linear bins
+        # ax.plot(
+        #     hists_bins[sim_ix],
+        #     hist,
+        #     label=r"$%d$" % Sims.O_per_chain[sim_ix],
+        #     alpha=leap.plot.ALPHA,
+        #     rasterized=False,
+        # )
+    ax.set_xscale("log", base=10, subs=np.arange(2, 10))
+    ax.set_yscale("log", base=10, subs=np.arange(2, 10))
+    ax.set(
+        xlabel=xlabel,
+        ylabel=ylabel,
+        xlim=xlim,
+        ylim=ylim,
+    )
+    legend = ax.legend(
+        title=legend_title(r"\pm", "bulk"),
+        loc="upper right",
+        ncol=n_legend_cols,
+        **mdtplt.LEGEND_KWARGS_XSMALL,
+    )
+    legend.get_title().set_multialignment("center")
+    pdf.savefig()
+    plt.close()
+
+    # Plot lifetime histograms for each bin.
+    for pkt_ix, pkp_type in enumerate(pk_pos_types):
+        hists_layer_pkt = hists_layer[pkt_ix]
+        hist_layer_states_pkt = hist_layer_states[pkt_ix]
+        bin_edges_pkt = bin_edges[pkt_ix]
+
+        if pkp_type == "left":
+            surfq_sign = "+"
+        elif pkp_type == "right":
+            surfq_sign = "-"
+        else:
+            raise ValueError("Unknown `pkp_type`: '{}'".format(pkp_type))
+
+        for cix_pkt in clstr_ix_unq[pkt_ix]:
+            valid_clstr = clstr_ix[pkt_ix] == cix_pkt
+            if not np.any(valid_clstr):
+                raise ValueError(
+                    "No valid peaks for peak type '{}' and cluster index"
+                    " {}".format(pkp_type, cix_pkt)
+                )
+            clstr_bin_mids = data_clstr[pkp_col_ix][pkt_ix][valid_clstr]
+            clstr_position = np.mean(clstr_bin_mids)
+            clstr_sim_indices = data_clstr[1][pkt_ix][valid_clstr]
+            clstr_states = data_clstr[2][pkt_ix][valid_clstr]
+            if not np.array_equal(
+                clstr_sim_indices, np.unique(clstr_sim_indices)
+            ):
+                raise ValueError(
+                    "Different bins of the same simulation were assigned to"
+                    " the same cluster."
+                )
+
+            fig, ax = plt.subplots(clear=True)
+            for six, sim_ix in enumerate(clstr_sim_indices):
+                hist_state = clstr_states[six]
+                valid_hist = hist_layer_states_pkt[sim_ix] == hist_state
+                if np.count_nonzero(valid_hist) != 1:
+                    raise ValueError(
+                        "The number of valid histograms in the current cluster"
+                        " for the current simulation is not one"
+                    )
+                hist_ix = np.flatnonzero(valid_hist)[0]
+                hist = hists_layer_pkt[sim_ix][hist_ix]
+                ax.stairs(
+                    hist,
+                    hists_bins[sim_ix],
+                    fill=False,
+                    label=r"$%d$" % Sims.O_per_chain[sim_ix],
+                    color=colors[sim_ix],
+                    alpha=leap.plot.ALPHA,
+                    rasterized=False,
+                )
+                # Use for linear bins
+                # ax.plot(
+                #     hists_bins[sim_ix],
+                #     hist,
+                #     label=r"$%d$" % Sims.O_per_chain[sim_ix],
+                #     color=colors[sim_ix],
+                #     alpha=leap.plot.ALPHA,
+                #     rasterized=False,
+                # )
+            ax.set_xscale("log", base=10, subs=np.arange(2, 10))
+            ax.set_yscale("log", base=10, subs=np.arange(2, 10))
+            ax.set(
+                xlabel=xlabel,
+                ylabel=ylabel,
+                xlim=xlim,
+                ylim=ylim,
+            )
+            legend = ax.legend(
+                title=legend_title(surfq_sign, clstr_position),
+                loc="upper right",
+                ncol=n_legend_cols,
+                **mdtplt.LEGEND_KWARGS_XSMALL,
+            )
+            legend.get_title().set_multialignment("center")
+            pdf.savefig()
+            plt.close()
+
+print("Created {}".format(outfile))
+print("Done")
