59ebb76def2bd8ef99b2eb601c5e22b655cab0a1
andthum <56444687+andthum@users.noreply.github.com>
2024-01-12 18:22:34 +0100

    [plot_density_chain-length_dependence.py]: Add Plot of Specific Volume
    Include plots and and fits of the specific volume (inverse density) as
    function of the inverse chain length.

--- scripts/bulk_and_walls/gmx/energy/plot_density_chain-length_dependence.py
+++ scripts/bulk_and_walls/gmx/energy/plot_density_chain-length_dependence.py
@@ -16,6 +16,35 @@ import mdtools as mdt
 import mdtools.plot as mdtplt  # Load MDTools plot style  # noqa: F401
 import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.ticker import MultipleLocator
+from scipy.optimize import curve_fit
+
+# First-party libraries
+import lintf2_ether_ana_postproc as leap
+
+
+def fit_line(xdata, ydata, ydata_sd, start=0, stop=-1):
+    """
+    Fit the linear `ydata` as function of the linear `xdata` with a
+    straight line.
+    """
+    xdata = xdata[start:stop]
+    ydata = ydata[start:stop]
+    sd = ydata_sd[start:stop]
+    slope_guess = (ydata[-1] - ydata[0]) / (xdata[-1] - xdata[0])
+    intercept_guess = ydata[0] - slope_guess * xdata[0]
+    popt, pcov = curve_fit(
+        f=leap.misc.straight_line,
+        xdata=xdata,
+        ydata=ydata,
+        p0=(slope_guess, intercept_guess),
+        sigma=sd,
+        absolute_sigma=True,
+    )
+    perr = np.sqrt(np.diag(pcov))
+    fit = leap.misc.straight_line(xdata, *popt)
+    fit_quality = leap.misc.fit_goodness(ydata, fit)
+    return popt, perr, fit_quality
 
 
 # Input parameters.
@@ -49,19 +78,69 @@ infiles = [
     settings + "_" + system + "_energy.txt.gz" for settings in settings_lst
 ]
 n_infiles = len(infiles)
-xdata = [None for infile in infiles]
-ydata = [None for infile in infiles]
-ydata_sd = [None for infiles in infiles]
+xdata = [[None for infile in infiles] for i in range(2)]
+ydata = [[None for infile in infiles] for i in range(2)]
+ydata_sd = [[None for infile in infiles] for i in range(2)]
 for set_ix, infile in enumerate(infiles):
-    xdata[set_ix], ydata[set_ix], ydata_sd[set_ix] = np.loadtxt(
+    xdata[0][set_ix], ydata[0][set_ix], ydata_sd[0][set_ix] = np.loadtxt(
         infile, usecols=cols, unpack=True
     )
+    ydata[0][set_ix] /= 1e3  # kg/m^3 -> g/cm^3.
+    ydata_sd[0][set_ix] /= 1e3  # Std[c*A] = c * Std[A].
+
+    # Calculate the specific volume (= inverse density).
+    xdata[1][set_ix] = 1 / xdata[0][set_ix]
+    ydata[1][set_ix] = 1 / ydata[0][set_ix]
+    # Propagation of uncertainty.
+    # https://en.wikipedia.org/wiki/Propagation_of_uncertainty#Example_formulae
+    # Std[1/A] = 1/|A| * Std[A]/|A| = Std[A]/A^2
+    ydata_sd[1][set_ix] = ydata_sd[0][set_ix] / ydata[0][set_ix] ** 2
+    # Sort data in ascending x order.
+    xdata[1][set_ix] = xdata[1][set_ix][::-1]
+    ydata[1][set_ix] = ydata[1][set_ix][::-1]
+    ydata_sd[1][set_ix] = ydata_sd[1][set_ix][::-1]
+
+    if set_ix == 0:
+        ydata_min = np.min(ydata[1][set_ix])
+        ydata_max = np.max(ydata[1][set_ix])
+    else:
+        ydata_min = min(np.min(ydata[1][set_ix]), ydata_min)
+        ydata_max = max(np.max(ydata[1][set_ix]), ydata_max)
+ydata_max_diff = ydata_max - ydata_min
+
+
+print("Fitting straight line...")
+# Fit straight line to specific volume as function of inverse chain
+# length.
+fit_starts = (0,)
+fit_stops = (len(xdata[1][0]),)
+popts = np.full((n_infiles, len(fit_starts), 2), np.nan, dtype=np.float64)
+perrs = np.full_like(popts, np.nan)
+fit_quality = np.full_like(popts, np.nan)
+for set_ix in range(n_infiles):
+    for fit_ix, start in enumerate(fit_starts):
+        (
+            popts[set_ix, fit_ix],
+            perrs[set_ix, fit_ix],
+            fit_quality[set_ix, fit_ix],
+        ) = fit_line(
+            xdata=xdata[1][set_ix],
+            ydata=ydata[1][set_ix],
+            ydata_sd=ydata_sd[1][set_ix],
+            start=start,
+            stop=fit_stops[fit_ix],
+        )
 
 
 print("Creating plot(s)...")
-xlabel = r"Ether Oxygens per Chain $n_{EO}$"
-xlim_lin = (0, 135)
-xlim_log = (1, 200)
+xlabels = (
+    r"Ether Oxygens per Chain $n_{EO}$",
+    r"Inverse Chain Length $1 / n_{EO}$",
+)
+ylabels = (r"Density / g cm$^{-3}$", r"Specific Volume / cm$^3$ g$^{-1}$")
+linestyles_data = (None, "none")
+xlims_lin = [(0, 135), (0.00, 0.52)]
+xlims_log = [(1, 200), (6e-3, 6e-1)]
 legend_title = r"$r = 0.05$" + "\n" + "T / K"
 markers = ("o", "s")
 if len(markers) != n_infiles:
@@ -69,40 +148,136 @@ if len(markers) != n_infiles:
         "`len(markers)` ({}) != `n_infiles`"
         " ({})".format(len(markers), n_infiles)
     )
+linestyles = ("dashed",)
+if len(linestyles) != len(fit_starts):
+    raise ValueError(
+        "`len(linestyles)` ({}) != `len(fit_starts)`"
+        " ({})".format(len(linestyles), len(fit_starts))
+    )
 
 mdt.fh.backup(outfile)
 with PdfPages(outfile) as pdf:
+    for data_ix, xlabel in enumerate(xlabels):
+        fig, ax = plt.subplots(clear=True)
+        for set_ix, temp in enumerate(temps):
+            lines = ax.errorbar(
+                xdata[data_ix][set_ix],
+                ydata[data_ix][set_ix],
+                yerr=ydata_sd[data_ix][set_ix],
+                label=r"$%d$" % temp,
+                marker=markers[set_ix],
+                linestyle=linestyles_data[data_ix],
+            )
+            if data_ix == 1:  # Specific volume.
+                # Fits.
+                for fit_ix, popt in enumerate(popts[set_ix]):
+                    x = xdata[data_ix][set_ix][
+                        fit_starts[fit_ix] : fit_stops[fit_ix]
+                    ]
+                    fit = leap.misc.straight_line(x, *popt)
+                    ax.plot(
+                        x,
+                        fit,
+                        color=lines[0].get_color(),
+                        linestyle=linestyles[fit_ix],
+                    )
+                    x_pos = x[-1] - 0.03 * xlims_lin[data_ix][-1]
+                    y_pos = leap.misc.straight_line(x_pos, *popt)
+                    y_offset = 0.02 * ydata_max_diff
+                    ax.text(
+                        x_pos,
+                        y_pos + y_offset if set_ix == 0 else y_pos - y_offset,
+                        r"$%.3f n_{EO} %+.3f$" % tuple(popt),
+                        rotation=np.rad2deg(np.arctan(popt[0])),
+                        rotation_mode="anchor",
+                        transform_rotates_text=True,
+                        horizontalalignment="right",
+                        verticalalignment="bottom" if set_ix == 0 else "top",
+                        linespacing=5,
+                        fontsize="x-small",
+                    )
+        ax.set(xlabel=xlabel, ylabel=ylabels[data_ix], xlim=xlims_lin[data_ix])
+        legend = ax.legend(title=legend_title, loc="lower right")
+        legend.get_title().set_multialignment("center")
+        pdf.savefig()
+        if data_ix == 1:  # Specific volume.
+            # Cutout.
+            ax.relim()
+            ax.autoscale()
+            ax.set(xlim=(0.004, 0.036), ylim=(0.790, 0.895))
+            ax.xaxis.set_major_locator(MultipleLocator(0.01))
+            legend = ax.legend(title=legend_title, loc="best")
+            legend.get_title().set_multialignment("center")
+            pdf.savefig()
+        # Log scale x.
+        ax.relim()
+        ax.autoscale()
+        ax.set_xscale("log", base=10, subs=np.arange(2, 10))
+        ax.set_xlim(xlims_log[data_ix])
+        pdf.savefig()
+        # Log scale y.
+        ax.relim()
+        ax.autoscale()
+        ax.set_xscale("linear")
+        ax.set_yscale("log", base=10, subs=np.arange(2, 10))
+        ax.set_xlim(xlims_lin[data_ix])
+        pdf.savefig()
+        # Log scale xy.
+        ax.relim()
+        ax.autoscale()
+        ax.set_xscale("log", base=10, subs=np.arange(2, 10))
+        ax.set_xlim(xlims_log[data_ix])
+        pdf.savefig()
+        plt.close()
+
+    # Print fit parameters.
     fig, ax = plt.subplots(clear=True)
+    ax.axis("off")
+    fontsize = "xx-small"
+    x_offset = 0.10
+    y_offset = 0.03
+    x_pos = 0.07
+    y_pos = 0.97
+    text = "Fit Parameters, " + legend_title.split("\n")[0]
+    fig.text(x_pos, y_pos, text, fontsize=fontsize)
     for set_ix, temp in enumerate(temps):
-        ax.errorbar(
-            xdata[set_ix],
-            ydata[set_ix],
-            yerr=ydata_sd[set_ix],
-            label=r"$%d$" % temp,
-            marker=markers[set_ix],
-        )
-    ax.set(xlabel=xlabel, ylabel=r"Density / kg m$^{-3}$", xlim=xlim_lin)
-    legend = ax.legend(title=legend_title)
-    legend.get_title().set_multialignment("center")
-    pdf.savefig()
-    # Log scale x.
-    ax.relim()
-    ax.autoscale()
-    ax.set_xscale("log", base=10, subs=np.arange(2, 10))
-    ax.set_xlim(xlim_log)
-    pdf.savefig()
-    # Log scale y.
-    ax.relim()
-    ax.autoscale()
-    ax.set_xscale("linear")
-    ax.set_yscale("log", base=10, subs=np.arange(2, 10))
-    ax.set_xlim(xlim_lin)
-    pdf.savefig()
-    # Log scale xy.
-    ax.relim()
-    ax.autoscale()
-    ax.set_xscale("log", base=10, subs=np.arange(2, 10))
-    ax.set_xlim(xlim_log)
+        y_pos -= 2 * y_offset
+        text = "$%d$ K" % temp
+        fig.text(x_pos, y_pos, text, fontsize=fontsize)
+        for fit_ix, popt in enumerate(popts[set_ix]):
+            if fit_ix > 0:
+                y_pos -= y_offset
+            text = r"Fit Region $%d$" % fit_ix
+            fig.text(x_pos + x_offset, y_pos, text, fontsize=fontsize)
+            for param_ix, param in enumerate(popt):
+                if param_ix == 0:
+                    text = "Slope:"
+                elif param_ix == 1:
+                    text = "Intercept:"
+                else:
+                    raise ValueError(
+                        "Unknown parameter index: {}".format(param_ix)
+                    )
+                y_pos -= y_offset
+                fig.text(x_pos + x_offset, y_pos, text, fontsize=fontsize)
+                text = r"$%+16.9e \pm %16.9e$" % (
+                    param,
+                    perrs[set_ix, fit_ix, param_ix],
+                )
+                fig.text(x_pos + 3 * x_offset, y_pos, text, fontsize=fontsize)
+            for qual_ix, quality in enumerate(fit_quality[set_ix, fit_ix]):
+                if qual_ix == 0:
+                    text = r"$R^2$:"
+                elif param_ix == 1:
+                    text = "RMSE:"
+                else:
+                    raise ValueError(
+                        "Unknown quality index: {}".format(qual_ix)
+                    )
+                y_pos -= y_offset
+                fig.text(x_pos + x_offset, y_pos, text, fontsize=fontsize)
+                text = r"$%+16.9e$" % quality
+                fig.text(x_pos + 3 * x_offset, y_pos, text, fontsize=fontsize)
     pdf.savefig()
     plt.close()
 
