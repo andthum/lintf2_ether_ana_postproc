73441fed9511c82c8bb4dcc148f13edeca8a0619
andthum <56444687+andthum@users.noreply.github.com>
2024-01-13 15:50:39 +0100

    [plot_density_chain-length_dependence.py]: Include Data From Literature

--- scripts/bulk_and_walls/gmx/energy/plot_density_chain-length_dependence.py
+++ scripts/bulk_and_walls/gmx/energy/plot_density_chain-length_dependence.py
@@ -73,6 +73,91 @@ cols = (
 )
 
 
+# Densities from literature.
+
+# Becht et al., Kolloid-Zeitschrift und Zeitschrift für Polymere, 1967,
+# 216, 1, 150-158.
+# Values taken from Figure 4.
+# Neat, OH-terminated PEO.
+n_eo = [2.00, 3.00, 5.24, 9.67, 13.53, 45.99, 145.87, 216.24, 369.46]
+n_eo = np.asarray(n_eo)
+Becht_1967_353K = [
+    1.06673e00,
+    1.06812e00,
+    1.07015e00,
+    1.07136e00,
+    1.07227e00,
+    1.07998e00,
+    1.07363e00,
+    1.07327e00,
+    1.07292e00,
+]
+Becht_1967_353K = np.asarray(Becht_1967_353K)
+Becht_1967_353K_xdata = np.asarray([n_eo, 1 / n_eo])
+Becht_1967_353K_ydata = np.asarray([Becht_1967_353K, 1 / Becht_1967_353K])
+
+# Zhang et al., J. Phys. Chem. B, 2014, 118, 19, 5144–5153.
+# Values taken from Table S1 in SI.
+# r = Li/EO ~ 1/20 = 0.05
+# Actual r values:  0.0625,  0.0417,  0.0625,  0.0500
+# 1/r = EO/Li:     16.00  , 18.00  , 16.00  , 20.00
+n_eo = np.asarray([2, 3, 4, 5])
+Zhang_2014_303K = np.asarray([1.0510, 1.1049, 1.1610, 1.1550])
+Zhang_2014_303K_xdata = np.asarray([n_eo, 1 / n_eo])
+Zhang_2014_303K_ydata = np.asarray([Zhang_2014_303K, 1 / Zhang_2014_303K])
+
+# Villaluenga et al., J. Electrochem. Soc., 2018, 165, 11, A2766-A2773.
+# Same as Pesko et al., J. Electrochem. Soc., 2017, 164, 11,
+# E3569-E3575.
+# Values taken from Table 1.
+# Actual r values:  0.06
+# 1/r = EO/Li:     16.68
+n_eo = np.asarray([112.45])
+Villaluenga_2018_363K = np.asarray([1.230])
+Villaluenga_2018_363K_xdata = np.asarray([n_eo, 1 / n_eo])
+Villaluenga_2018_363K_ydata = np.asarray(
+    [Villaluenga_2018_363K, 1 / Villaluenga_2018_363K]
+)
+
+# Densities from experiments.
+experiments_xdata = np.concatenate(
+    [Zhang_2014_303K_xdata, Villaluenga_2018_363K_xdata], axis=-1
+)
+experiments_ydata = np.concatenate(
+    [Zhang_2014_303K_ydata, Villaluenga_2018_363K_ydata], axis=-1
+)
+
+# Chattoraj et al., J. Phys. Chem. B, 2015, 119, 6786−6791.
+# APPLE&P force field.
+# Value taken from text (Section "MOLECULAR DYNAMICS SIMULATIONS").
+n_eo = np.asarray([54])
+Chattoraj_2015_423K = np.asarray([1.127])
+Chattoraj_2015_423K_xdata = np.asarray([n_eo, 1 / n_eo])
+Chattoraj_2015_423K_ydata = np.asarray(
+    [Chattoraj_2015_423K, 1 / Chattoraj_2015_423K]
+)
+
+# Diddens et al., J. Electrochem. Soc., 2017, 164, 1, E3225-E3231.
+# APPLE&P force field.
+# Value taken from simulations of Diddo Diddens.
+# See also Supplementary of Thum et al., J. Phys. Chem. C, 2021, 125,
+# 25392−25403.
+n_eo = np.asarray([64])
+Diddens_2017_423K = np.asarray([1.15096])
+Diddens_2017_423K_xdata = np.asarray([n_eo, 1 / n_eo])
+Diddens_2017_423K_ydata = np.asarray(
+    [Diddens_2017_423K, 1 / Diddens_2017_423K]
+)
+
+# Densities from MD simulations with the APPLE&P force field.
+apple_p_xdata = np.concatenate(
+    [Chattoraj_2015_423K_xdata, Diddens_2017_423K_xdata], axis=-1
+)
+apple_p_ydata = np.concatenate(
+    [Chattoraj_2015_423K_ydata, Diddens_2017_423K_ydata], axis=-1
+)
+
+
 print("Reading data...")
 infiles = [
     settings + "_" + system + "_energy.txt.gz" for settings in settings_lst
@@ -141,7 +226,7 @@ ylabels = (r"Density / g cm$^{-3}$", r"Specific Volume / cm$^3$ g$^{-1}$")
 linestyles_data = (None, "none")
 xlims_lin = [(0, 135), (0.00, 0.52)]
 xlims_log = [(1, 200), (6e-3, 6e-1)]
-legend_title = r"$r = 0.05$" + "\n" + "T / K"
+legend_title = r"$r = 0.05$"
 markers = ("o", "s")
 if len(markers) != n_infiles:
     raise ValueError(
@@ -164,7 +249,7 @@ with PdfPages(outfile) as pdf:
                 xdata[data_ix][set_ix],
                 ydata[data_ix][set_ix],
                 yerr=ydata_sd[data_ix][set_ix],
-                label=r"$%d$" % temp,
+                label=r"$%d$ K" % temp,
                 marker=markers[set_ix],
                 linestyle=linestyles_data[data_ix],
             )
@@ -196,8 +281,30 @@ with PdfPages(outfile) as pdf:
                         linespacing=5,
                         fontsize="x-small",
                     )
+        ax.errorbar(
+            experiments_xdata[data_ix],
+            experiments_ydata[data_ix],
+            yerr=None,
+            label=r"Exp. ($r \approx 0.05$)",
+            marker="p",
+            color="tab:cyan",
+            linestyle="none",
+        )
+        ax.errorbar(
+            apple_p_xdata[data_ix],
+            apple_p_ydata[data_ix],
+            yerr=None,
+            label="APPLE&P",
+            marker="D",
+            color="tab:red",
+            linestyle="none",
+        )
         ax.set(xlabel=xlabel, ylabel=ylabels[data_ix], xlim=xlims_lin[data_ix])
-        legend = ax.legend(title=legend_title, loc="lower right")
+        legend = ax.legend(
+            title=legend_title,
+            loc="lower right",
+            **mdtplt.LEGEND_KWARGS_XSMALL,
+        )
         legend.get_title().set_multialignment("center")
         pdf.savefig()
         if data_ix == 1:  # Specific volume.
@@ -206,7 +313,9 @@ with PdfPages(outfile) as pdf:
             ax.autoscale()
             ax.set(xlim=(0.004, 0.036), ylim=(0.790, 0.895))
             ax.xaxis.set_major_locator(MultipleLocator(0.01))
-            legend = ax.legend(title=legend_title, loc="best")
+            legend = ax.legend(
+                title=legend_title, loc="best", **mdtplt.LEGEND_KWARGS_XSMALL
+            )
             legend.get_title().set_multialignment("center")
             pdf.savefig()
         # Log scale x.
@@ -238,7 +347,7 @@ with PdfPages(outfile) as pdf:
     y_offset = 0.03
     x_pos = 0.07
     y_pos = 0.97
-    text = "Fit Parameters, " + legend_title.split("\n")[0]
+    text = "Fit Parameters, " + legend_title
     fig.text(x_pos, y_pos, text, fontsize=fontsize)
     for set_ix, temp in enumerate(temps):
         y_pos -= 2 * y_offset
