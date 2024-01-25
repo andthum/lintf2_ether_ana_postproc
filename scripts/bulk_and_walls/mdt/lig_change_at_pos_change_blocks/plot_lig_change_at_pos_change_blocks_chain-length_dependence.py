44d5ab7962bd60351529c9ae834a9fa4fcacfda9
andthum <56444687+andthum@users.noreply.github.com>
2024-01-25 17:16:09 +0100

    [plot_lig_change_at_pos_change_blocks_chain-length_dependence.py]: Create (WIP)

--- scripts/bulk_and_walls/mdt/lig_change_at_pos_change_blocks/plot_lig_change_at_pos_change_blocks_chain-length_dependence.py
+++ scripts/bulk_and_walls/mdt/lig_change_at_pos_change_blocks/plot_lig_change_at_pos_change_blocks_chain-length_dependence.py
@@ -29,6 +29,56 @@ from scipy.cluster.hierarchy import dendrogram
 import lintf2_ether_ana_postproc as leap
 
 
+def legend_title(surfq_sign):
+    r"""
+    Create a legend title string.
+
+    Parameters
+    ----------
+    surfq_sign : {"+", "-", r"\pm"}
+        The sign of the surface charge.
+
+    Returns
+    -------
+    title : str
+        The legend title.
+
+    Notes
+    -----
+    This function relies on global variables!
+    """
+    if surfq_sign not in ("+", "-", r"\pm"):
+        raise ValueError("Unknown `surfq_sign`: '{}'".format(surfq_sign))
+    return (
+        r"$\sigma_s = "
+        + surfq_sign
+        + r" %.2f$ $e$/nm$^2$, " % Sims.surfqs[0]
+        + r"$r = %.2f$" % Sims.Li_O_ratios[0]
+        + "\n"
+    )
+
+
+def equalize_yticks(ax):
+    """
+    Equalize y-ticks so that plots can be better stacked together.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        The :class:`~matplotlib.axes.Axes` for which to equalize the y
+        ticks.
+    """
+    ylim = np.asarray(ax.get_ylim())
+    ylim_diff = ylim[-1] - ylim[0]
+    yticks = np.asarray(ax.get_yticks())
+    yticks_valid = (yticks >= ylim[0]) & (yticks <= ylim[-1])
+    yticks = yticks[yticks_valid]
+    if ylim_diff >= 10 and ylim_diff < 20:
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    if np.all(yticks >= 0) and np.all(yticks < 10) and ylim_diff > 2:
+        ax.yaxis.set_major_formatter(FormatStrFormatter("%.1f"))
+
+
 # Input parameters.
 parser = argparse.ArgumentParser(
     description=(
@@ -185,14 +235,20 @@ trans_types = ("successful", "unsuccessful")
 #   * Toward, Unsuccessful (+/- Std).
 #   * Away, Successful (+/- Std).
 #   * Away, Unsuccessful (+/- Std).
+n_data_per_plot = len(trans_drctn_types) * len(trans_types)
 n_data = 1  # Barrier positions.
-n_data += len(rows) * len(trans_drctn_types) * len(trans_types) * 2
-n_data -= len(trans_drctn_types) * len(trans_types)  # No Std for Type 5
+n_data += len(rows) * n_data_per_plot * 2
+n_data -= n_data_per_plot  # No Std for Type 5
 n_data += 2  # Plot type 6.
 ydata = [
     [[[] for sim in Sims.sims] for pkp_type in pk_pos_types]
     for dat_ix in range(n_data)
 ]
+# Boolean array that indicates which of the data are standard deviations
+data_is_sd = np.zeros(n_data, dtype=bool)
+data_is_sd[2 : 3 * n_data_per_plot * 2 + 1 : 2] = True
+data_is_sd[-n_data_per_plot * 2 + 1 :: 2] = True
+n_data_not_sd = n_data - np.count_nonzero(data_is_sd)
 
 ana_parent_dir = os.path.join(tool, analysis + analysis_suffix)
 for sim_ix, Sim in enumerate(Sims.sims):
@@ -382,3 +438,237 @@ for pkt_ix, n_pks_per_sim_pkt in enumerate(n_pks_per_sim):
 
 
 print("Creating plot(s)...")
+xlabel = r"Ether Oxygens per Chain $n_{EO}$"
+xlim = (1, 200)
+if args.common_ylim:
+    if cmp1 == "Li":
+        ylims = [
+            (0, 3.6),  # Barrier positions [nm].
+        ]
+    else:
+        raise NotImplementedError(
+            "Common ylim not implemented for compounds other than Li"
+        )
+    ylims += [(None, None) for dat_ix in range(n_data_not_sd - 1)]
+else:
+    ylims = tuple((None, None) for dat_ix in range(n_data_not_sd))
+
+mdt.fh.backup(outfile)
+with PdfPages(outfile) as pdf:
+    ####################################################################
+    # Plot ligand exchange information for each free-energy barrier.
+
+    ####################################################################
+    # Plot barrier positions.
+    ylabel = "Distance to Electrode / nm"
+    legend_title_suffix = (
+        r"$F_{"
+        + leap.plot.ATOM_TYPE2DISPLAY_NAME[cmp1]
+        + r"}$ Maxima Positions / nm"
+    )
+    linestyles_comb = ("solid", "dashed")
+    markers = ("<", ">")
+
+    n_clstrs_plot = np.max(n_clstrs)
+    cmap = plt.get_cmap()
+    c_vals_sep = np.arange(n_clstrs_plot)
+    c_norm_sep = n_clstrs_plot - 1
+    c_vals_sep_normed = c_vals_sep / c_norm_sep
+    colors_sep = cmap(c_vals_sep_normed)
+
+    for col_ix, yd_col in enumerate(
+        ydata[pkp_col_ix : pkp_col_ix + 1], start=pkp_col_ix
+    ):
+        # Peaks at left and right electrode combined in one plot.
+        fig_comb, ax_comb = plt.subplots(clear=True)
+
+        for pkt_ix, pkp_type in enumerate(pk_pos_types):
+            yd_pkt = yd_col[pkt_ix]
+
+            # Peaks at left and right electrode in separate plots.
+            fig_sep, ax_sep = plt.subplots(clear=True)
+            ax_sep.set_prop_cycle(color=colors_sep)
+
+            c_vals_comb = c_vals_sep + 0.5 * pkt_ix
+            c_norm_comb = c_norm_sep + 0.5 * (len(pk_pos_types) - 1)
+            c_vals_comb_normed = c_vals_comb / c_norm_comb
+            colors_comb = cmap(c_vals_comb_normed)
+            ax_comb.set_prop_cycle(color=colors_comb)
+
+            for cix_pkt in clstr_ix_unq[pkt_ix]:
+                if cix_pkt >= n_clstrs_plot:
+                    break
+
+                valid = clstr_ix[pkt_ix] == cix_pkt
+                if not np.any(valid):
+                    raise ValueError(
+                        "No valid peaks for peak type '{}' and cluster index"
+                        " {}".format(pkp_type, cix_pkt)
+                    )
+
+                if pkp_type == "left":
+                    label_sep = r"$<%.2f$" % clstr_bounds[pkt_ix][cix_pkt]
+                    label_comb = r"$+, " + label_sep[1:]
+                elif pkp_type == "right":
+                    label_sep = r"$<%.2f$" % clstr_bounds[pkt_ix][cix_pkt]
+                    label_comb = r"$-, " + label_sep[1:]
+                else:
+                    raise ValueError(
+                        "Unknown `pkp_type`: '{}'".format(pkp_type)
+                    )
+                ax_sep.plot(
+                    xdata[pkt_ix][valid],
+                    yd_pkt[valid],
+                    linestyle="solid",
+                    marker=markers[pkt_ix],
+                    label=label_sep,
+                )
+                ax_comb.plot(
+                    xdata[pkt_ix][valid],
+                    yd_pkt[valid],
+                    linestyle=linestyles_comb[pkt_ix],
+                    marker=markers[pkt_ix],
+                    label=label_comb,
+                )
+
+            if pkp_type == "left":
+                legend_title_sep = legend_title(r"+")
+            elif pkp_type == "right":
+                legend_title_sep = legend_title(r"-")
+            else:
+                raise ValueError("Unknown `pkp_type`: '{}'".format(pkp_type))
+            ax_sep.set_xscale("log", base=10, subs=np.arange(2, 10))
+            ax_sep.set(
+                xlabel=xlabel, ylabel=ylabel, xlim=xlim, ylim=ylims[col_ix]
+            )
+            equalize_yticks(ax_sep)
+            legend_sep = ax_sep.legend(
+                title=legend_title_sep + legend_title_suffix,
+                ncol=2,
+                **mdtplt.LEGEND_KWARGS_XSMALL,
+            )
+            legend_sep.get_title().set_multialignment("center")
+            pdf.savefig(fig_sep)
+            plt.close(fig_sep)
+
+        ax_comb.set_xscale("log", base=10, subs=np.arange(2, 10))
+        ax_comb.set(
+            xlabel=xlabel, ylabel=ylabel, xlim=xlim, ylim=ylims[col_ix]
+        )
+        equalize_yticks(ax_comb)
+        legend_comb = ax_comb.legend(
+            title=legend_title(r"\pm") + legend_title_suffix,
+            ncol=2,
+            **mdtplt.LEGEND_KWARGS_XSMALL,
+        )
+        legend_comb.get_title().set_multialignment("center")
+        pdf.savefig(fig_comb)
+        plt.close(fig_comb)
+
+    ####################################################################
+    # Plot clustering results.
+    for pkt_ix, pkp_type in enumerate(pk_pos_types):
+        if pkp_type == "left":
+            legend_title_clstrng = legend_title("+")
+        elif pkp_type == "right":
+            legend_title_clstrng = legend_title("-")
+        else:
+            raise ValueError("Unknown `pkp_type`: '{}'".format(pkp_type))
+        cmap_norm = plt.Normalize(vmin=0, vmax=np.max(n_clstrs) - 1)
+
+        # Dendrogram.
+        fig, ax = plt.subplots(clear=True)
+        dendrogram(
+            linkage_matrices[pkt_ix],
+            ax=ax,
+            distance_sort="ascending",
+            color_threshold=clstr_dist_thresh[pkt_ix],
+        )
+        ax.axhline(
+            clstr_dist_thresh[pkt_ix],
+            color="tab:gray",
+            linestyle="dashed",
+            label=r"Threshold $%.2f$ nm" % clstr_dist_thresh[pkt_ix],
+        )
+        ax.set(
+            xlabel="Peak Number",
+            ylabel="Peak Distance / nm",
+            ylim=(0, ylims[pkp_col_ix][-1]),
+        )
+        legend = ax.legend(
+            title=(
+                legend_title_clstrng
+                + r"$F_{"
+                + leap.plot.ATOM_TYPE2DISPLAY_NAME[cmp1]
+                + r"}$ Maxima"
+            ),
+            loc="best",
+            **mdtplt.LEGEND_KWARGS_XSMALL,
+        )
+        legend.get_title().set_multialignment("center")
+        pdf.savefig()
+        plt.close()
+
+        # Scatter plot: Peak Positions vs. Peak Positions.
+        fig, ax = plt.subplots(clear=True)
+        for cix_pkt in clstr_ix_unq[pkt_ix]:
+            if cix_pkt >= n_clstrs_plot:
+                break
+            valid = clstr_ix[pkt_ix] == cix_pkt
+            if not np.any(valid):
+                raise ValueError(
+                    "No valid peaks for peak type '{}' and cluster index"
+                    " {}".format(pkp_type, cix_pkt)
+                )
+            ax.scatter(
+                ydata[pkp_col_ix][pkt_ix][valid],
+                ydata[pkp_col_ix][pkt_ix][valid],
+                color=cmap(cmap_norm(cix_pkt)),
+                marker=markers[pkt_ix],
+                label="$<%.2f$" % clstr_bounds[pkt_ix][cix_pkt],
+            )
+        ax.set(
+            xlabel=ylabel,
+            ylabel=ylabel,
+            xlim=ylims[pkp_col_ix],
+            ylim=ylims[pkp_col_ix],
+        )
+        legend = ax.legend(
+            title=legend_title_clstrng + legend_title_suffix,
+            ncol=2,
+            loc="upper left",
+            **mdtplt.LEGEND_KWARGS_XSMALL,
+        )
+        legend.get_title().set_multialignment("center")
+        pdf.savefig()
+        plt.close()
+
+        # Scatter plot: Peak Positions vs. `xdata`.
+        fig, ax = plt.subplots(clear=True)
+        for cix_pkt in clstr_ix_unq[pkt_ix]:
+            if cix_pkt >= n_clstrs_plot:
+                break
+            valid = clstr_ix[pkt_ix] == cix_pkt
+            if not np.any(valid):
+                raise ValueError(
+                    "No valid peaks for peak type '{}' and cluster index"
+                    " {}".format(pkp_type, cix_pkt)
+                )
+            ax.scatter(
+                xdata[pkt_ix][valid],
+                ydata[pkp_col_ix][pkt_ix][valid],
+                color=cmap(cmap_norm(cix_pkt)),
+                marker=markers[pkt_ix],
+                label="$<%.2f$" % clstr_bounds[pkt_ix][cix_pkt],
+            )
+        ax.set_xscale("log", base=10, subs=np.arange(2, 10))
+        ax.set(xlabel=xlabel, ylabel=ylabel, xlim=xlim, ylim=ylims[pkp_col_ix])
+        equalize_yticks(ax)
+        legend = ax.legend(
+            title=legend_title_clstrng + legend_title_suffix,
+            ncol=2,
+            **mdtplt.LEGEND_KWARGS_XSMALL,
+        )
+        legend.get_title().set_multialignment("center")
+        pdf.savefig()
+        plt.close()
