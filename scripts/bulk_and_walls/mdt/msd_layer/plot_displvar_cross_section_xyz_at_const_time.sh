ab2c17c3ac7043f4ac846382769825d544311c33
andthum <56444687+andthum@users.noreply.github.com>
2024-01-31 09:04:38 +0100

    [plot_displvar_cross_section_xyz_at_const_time.sh]: Create
    Create a new Bash script
    `scripts/bulk_and_walls/mdt/msd_layer/plot_displvar_cross_section_xyz_at_const_time.sh`
    that loops over all surface-simulation directories and runs the Python
    script `plot_displvar_cross_section_xyz_at_const_time.py` to plot the
    x-, y- and z-component of the mean displacement and the displacement
    variance as function of the initial particle position at a constant
    diffusion time.

---
+++ scripts/bulk_and_walls/mdt/msd_layer/plot_displvar_cross_section_xyz_at_const_time.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+
+settings="pr_nvt423_nh"
+top_path="${HOME}/ownCloud/WWU_Münster/Promotion/Simulationen/results/lintf2_peo/walls"
+cmp=Li
+flags=()
+
+########################################################################
+# Information and Usage Functions                                      #
+########################################################################
+
+information() {
+    echo "Loop over all surface-simulation directories and run the"
+    echo "Python script plot_displvar_cross_section_xyz_at_const_time.py"
+    echo "to plot the x-, y- and z-component of the mean displacement"
+    echo "and the displacement variance as function of the initial"
+    echo "particle position at a constant diffusion time."
+}
+
+usage() {
+    echo
+    echo "Usage:"
+    echo
+    echo "Optional arguments:"
+    echo "  -h    Show this help message and exit."
+    echo "  -e    String describing the used simulation settings."
+    echo "        Default: '${settings}'."
+    echo "  -t    Top-level simulation path containing all surface"
+    echo "        simulations.  Default: '${top_path}'."
+    echo "  -c    The compound for which to plot the displacements."
+    echo "        Default: '${cmp}'"
+    echo "  -f    Addtional options (besides --system, --settings and --cmp)"
+    echo "        to parse to"
+    echo "        'plot_displvar_cross_section_xyz_at_const_time.py' as one"
+    echo "        long, enquoted string.  See there for possible options."
+    echo "        Default: '${flags[*]}'"
+}
+
+########################################################################
+# Argument Parsing                                                     #
+########################################################################
+
+while getopts he:t:c:f: option; do
+    case ${option} in
+        # Optional arguments.
+        h)
+            information
+            usage
+            exit 0
+            ;;
+        e)
+            settings=${OPTARG}
+            ;;
+        t)
+            top_path=${OPTARG}
+            ;;
+        c)
+            cmp=${OPTARG}
+            ;;
+        f)
+            # See https://github.com/koalaman/shellcheck/wiki/SC2086#exceptions
+            # and https://github.com/koalaman/shellcheck/wiki/SC2206
+            # shellcheck disable=SC2206
+            flags=(${OPTARG})
+            ;;
+        # Handling of invalid options or missing arguments.
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [[ ! -d ${top_path} ]]; then
+    echo "ERROR: No such directory: '${top_path}'"
+    exit 1
+fi
+
+########################################################################
+# Main Part                                                            #
+########################################################################
+
+cwd=$(pwd) # Current working directory.
+
+script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+project_root=$(readlink -e "${script_dir}/../../../.." || exit)
+py_exe=".venv/bin/python3"
+py_exe="${project_root}/${py_exe}"
+py_script=$(readlink -e "${script_dir}/plot_displvar_cross_section_xyz_at_const_time.py" || exit)
+if [[ ! -d ${script_dir} ]]; then
+    echo "ERROR: No such directory '${script_dir}'"
+    exit 1
+fi
+if [[ ! -d ${project_root} ]]; then
+    echo "ERROR: No such directory '${project_root}'"
+    exit 1
+fi
+if [[ ! -x ${py_exe} ]]; then
+    echo "ERROR: No such executable '${py_exe}'"
+    exit 1
+fi
+if [[ ! -f ${py_script} ]]; then
+    echo "ERROR: No such file '${py_script}'"
+    exit 1
+fi
+
+analysis="msd_layer"
+tool="mdt"
+
+cd "${top_path}" || exit
+for surfq in q[0-9]*; do
+    echo
+    echo "============================================================="
+    echo "${surfq}"
+    if [[ ! -d ${surfq} ]]; then
+        echo "WARNING: No such directory: '${surfq}'"
+        continue
+    fi
+    cd "${surfq}" || exit
+
+    for system in lintf2_[gp]*[0-9]*_[0-9]*-[0-9]*_gra_"${surfq}"_sc80; do
+        echo
+        echo "${system}"
+        if [[ ! -d ${system} ]]; then
+            echo "WARNING: No such directory: '${system}'"
+            continue
+        fi
+        cd "${system}" || exit
+
+        sim_dir="${settings}_${system}"
+        if [[ ${system} != *_gra_* ]]; then
+            sim_dir="07_${sim_dir}"
+        elif [[ ${system} == *_gra_q0_* ]]; then
+            sim_dir="09_${sim_dir}"
+        else
+            sim_dir="01_${sim_dir}"
+        fi
+        if [[ ! -d ${sim_dir} ]]; then
+            echo "WARNING: No such directory: '${sim_dir}'"
+            cd ../ || exit
+            continue
+        fi
+        cd "${sim_dir}" || exit
+
+        ana_dir="ana_${settings}_${system}/${tool}/${analysis}"
+        if [[ ! -d ${ana_dir} ]]; then
+            echo "WARNING: No such directory: '${ana_dir}'"
+            cd ../../ || exit
+            continue
+        fi
+        cd "${ana_dir}" || exit
+
+        ana_sub_dir="${analysis}_${cmp}"
+        if [[ ! -d ${ana_sub_dir} ]]; then
+            echo "WARNING: No such directory: '${ana_sub_dir}'"
+            cd ../../../../../ || exit
+            continue
+        fi
+        cd "${ana_sub_dir}" || exit
+
+        # shellcheck disable=SC2048,SC2086
+        ${py_exe} "${py_script}" \
+            --system "${system}" \
+            --settings "${settings}" \
+            --cmp "${cmp}" \
+            ${flags[*]} ||
+            exit
+
+        cd ../../../../../../ || exit
+    done
+
+    cd ../ || exit
+    echo "============================================================="
+done
+cd "${cwd}" || exit
+
+echo
+echo "Done"
